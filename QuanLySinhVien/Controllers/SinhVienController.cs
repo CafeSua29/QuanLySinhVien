603047eeca890a76bbd55454a3315b5e9eb905eb
camtyr <ducbuivan08@gmail.com>
2023-10-02 19:03:29 +0700

    add student

--- QuanLySinhVien/Controllers/SinhVienController.cs
+++ QuanLySinhVien/Controllers/SinhVienController.cs
@@ -3,13 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using QuanLySinhVien.Models;
 
 namespace QuanLySinhVien.Controllers
 {
     public class SinhVienController : Controller
     {
+        QuanLySinhVienEntities db = new QuanLySinhVienEntities();
         // GET: SinhVien
-        public ActionResult Index()
+        public ActionResult DanhSachSinhVien()
         {
             return View();
         }
@@ -17,6 +19,39 @@ namespace QuanLySinhVien.Controllers
         public ActionResult ChiTietSinhVien()
         {
             return View();
+        }
+        public ActionResult ThemSinhVien()
+        {
+            ViewBag.MaLop = new SelectList(LopBUS.DanhSachLop(),"MaLop", "TenLop");
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult ThemSinhVien(SinhVien model)
+        {
+            if(ModelState.IsValid)
+            {
+                var check = db.SinhViens.FirstOrDefault(x => x.MaSV == model.MaSV);
+                if (check == null)
+                {
+                    db.Configuration.ValidateOnSaveEnabled = false;
+                    db.SinhViens.Add(model);
+                    db.SaveChanges();
+                    return RedirectToAction("DanhSachSinhVien");
+                }
+                else
+                {
+                    ViewBag.MaLop = new SelectList(LopBUS.DanhSachLop(), "MaLop", "TenLop");
+                    TempData["Error"] = "Mã sinh viên đã tồn tại";
+                    return View(model);
+                }
+            }
+            else
+            {
+                ViewBag.MaLop = new SelectList(LopBUS.DanhSachLop(), "MaLop", "TenLop");
+                return View(model);
+            }
         }
     }
 }
---
+++ QuanLySinhVien/Models/LopBUS.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QuanLySinhVien.Models
+{
+    public class LopBUS
+    {
+        public static IEnumerable<Lop> DanhSachLop()
+        {
+            QuanLySinhVienEntities db = new QuanLySinhVienEntities();
+            IQueryable<Lop> dsLop = 
+                from ds in db.Lops
+                select ds;
+            return dsLop;
+        }
+    }
+}
